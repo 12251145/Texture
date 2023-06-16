2e475fbb09f4d8bb4a7b6130a2e7dc86075e1050
12251145 <96657571+12251145@users.noreply.github.com>
2023-06-16 18:58:36 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AsyncDisplayKit",
-            type: .dynamic,
             targets: ["AsyncDisplayKit"]),
     ],
     dependencies: [
